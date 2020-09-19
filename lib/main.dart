5af472d0aeae1c16fd011e36ec2471fae4750e79
shekharAggarwal <aggarwalshekhar99@gmail.com>
2020-09-19 20:49:29 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,413 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/model/movie.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MoveListView(),
+  ));
+}
+
+class MoveListView extends StatelessWidget {
+  final List<Movie> movieList = Movie.getMovies();
+
+  final List movies = ["Titanic", "Blade Runner", "Rambo"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      backgroundColor: Colors.blueGrey.shade900,
+      body: ListView.builder(
+          itemCount: movieList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Stack(
+              children: <Widget>[
+                movieCard(movieList[index], context),
+                Positioned(
+                    top: 10.0, child: movieImage(movieList[index].images[0])),
+              ],
+            );
+            // return Card(
+            //   elevation: 4.5,
+            //   color: Colors.white,
+            //   child: ListTile(
+            //     leading: CircleAvatar(
+            //       child: Container(
+            //         width: 200,
+            //         height: 200,
+            //         decoration: BoxDecoration(
+            //           image: DecorationImage(
+            //             image: NetworkImage(movieList[index].images[0]),
+            //             fit: BoxFit.cover,
+            //           ),
+            //           borderRadius: BorderRadius.circular(13.9),
+            //         ),
+            //         child: null,
+            //       ),
+            //     ),
+            //     trailing: Text("..."),
+            //     title: Text(movieList[index].title),
+            //     subtitle: Text(movieList[index].title),
+            //     onTap: () {
+            //       Navigator.push(
+            //           context,
+            //           MaterialPageRoute(
+            //               builder: (context) => MovieListViewDetails(
+            //                     movieName: movieList.elementAt(index).title,
+            //                     movie: movieList[index],
+            //                   )));
+            //     },
+            //   ),
+            // );
+          }),
+    );
+  }
+}
+
+Widget movieCard(Movie movie, BuildContext context) {
+  return InkWell(
+    child: Container(
+      margin: EdgeInsets.only(left: 60.0),
+      width: MediaQuery.of(context).size.width,
+      height: 120.0,
+      child: Card(
+        color: Colors.black45,
+        child: Padding(
+          padding: const EdgeInsets.only(
+              top: 8.0, bottom: 8.0, left: 54.0, right: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Flexible(
+                    child: Text(
+                      movie.title,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17.0,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Text(
+                    "Rating: ${movie.imdbRating} / 10",
+                    style: TextStyle(fontSize: 15.0, color: Colors.grey),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text("Released: ${movie.released}", style: mainTextStyle()),
+                  Text(movie.runtime, style: mainTextStyle()),
+                  Text(movie.rated, style: mainTextStyle())
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+    onTap: () => {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MovieListViewDetails(
+                    movieName: movie.title,
+                    movie: movie,
+                  )))
+    },
+  );
+}
+
+TextStyle mainTextStyle() {
+  return TextStyle(fontSize: 15.0, color: Colors.grey);
+}
+
+Widget movieImage(String imageUrl) {
+  return Container(
+    width: 100.0,
+    height: 100.0,
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      image: DecorationImage(
+          image: NetworkImage(imageUrl ??
+              'https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyYzgyOWEtNTY2NS00NjRjLWJiNDYtMWViMjg5MWZjYjgzXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg'),
+          fit: BoxFit.cover),
+    ),
+  );
+}
+
+class MovieListViewDetails extends StatelessWidget {
+  final String movieName;
+  final Movie movie;
+
+  const MovieListViewDetails({Key key, this.movieName, this.movie})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      body: ListView(
+        children: <Widget>[
+          MovieDetailsThumbnail(
+            thumbnail: movie.images[0],
+          ),
+          MovieDetailsHeaderWithPoster(
+            movie: movie,
+          ),
+           HorizontalLine(),
+          MovieDetailsCast(
+            movie: movie,
+          ),
+          HorizontalLine(),
+          MovieDetailsExtraPosters(
+            posters: movie.images,
+          ),
+        ],
+      ),
+      // body: Container(
+      //   child: RaisedButton(
+      //     child: Text("Go Back ${this.movie.director}"),
+      //     onPressed: () {
+      //       Navigator.pop(context);
+      //     },
+      //   ),
+      // ),
+    );
+  }
+}
+
+class MovieDetailsThumbnail extends StatelessWidget {
+  final String thumbnail;
+
+  const MovieDetailsThumbnail({Key key, this.thumbnail}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 170.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(thumbnail), fit: BoxFit.cover),
+              ),
+            ),
+            Icon(
+              Icons.play_circle_outline,
+              size: 100.0,
+              color: Colors.white,
+            )
+          ],
+        ),
+        Container(
+          height: 80.0,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0x00f5f5f5), Color(0xfff5f5f5)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class MovieDetailsHeaderWithPoster extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsHeaderWithPoster({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: <Widget>[
+          MoviePoster(poster: movie.images[0].toString()),
+          SizedBox(
+            width: 16.0,
+          ),
+          Expanded(child: MovieDetailsHeader(movie: movie)),
+        ],
+      ),
+    );
+  }
+}
+
+class MoviePoster extends StatelessWidget {
+  final String poster;
+
+  const MoviePoster({Key key, this.poster}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var borderRadius = BorderRadius.all(Radius.circular(10));
+    return Card(
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: Container(
+          width: MediaQuery.of(context).size.width / 4,
+          height: 160.0,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(poster), fit: BoxFit.cover)),
+        ),
+      ),
+    );
+  }
+}
+
+class MovieDetailsHeader extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsHeader({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "${movie.year} . ${movie.genre}".toUpperCase(),
+          style: TextStyle(fontWeight: FontWeight.w400, color: Colors.cyan),
+        ),
+        Text(
+          movie.title,
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 32.0),
+        ),
+        Text.rich(
+          TextSpan(
+              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 12.0),
+              children: <TextSpan>[
+                TextSpan(text: movie.plot),
+                TextSpan(
+                    text: "More...",
+                    style: TextStyle(color: Colors.indigoAccent))
+              ]),
+        ),
+      ],
+    );
+  }
+}
+
+class MovieDetailsCast extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsCast({Key key, this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: <Widget>[
+          MovieField(field: "Cast", value: movie.actors),
+          MovieField(field: "Directors", value: movie.director),
+          MovieField(field: "Awards", value: movie.awards)
+        ],
+      ),
+    );
+  }
+}
+
+class MovieField extends StatelessWidget {
+  final String field;
+  final String value;
+
+  const MovieField({Key key, this.field, this.value}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "$field : ",
+          style: TextStyle(
+              color: Colors.black38,
+              fontSize: 12.0,
+              fontWeight: FontWeight.w300),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 12.0,
+                fontWeight: FontWeight.w300),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HorizontalLine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      child: Container(
+        height: 0.5,
+        color: Colors.grey,
+      ),
+    );
+  }
+}
+
+class MovieDetailsExtraPosters extends StatelessWidget {
+  final List<String> posters;
+
+  const MovieDetailsExtraPosters({Key key, this.posters}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            "More Movie Posters".toUpperCase(),
+            style: TextStyle(fontSize: 14.0, color: Colors.black26),
+          ),
+        ),
+        Container(
+          height: 170.0,
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (context, index) => SizedBox(
+              width: 8.0,
+            ),
+            itemCount: posters.length,
+            itemBuilder: (context, index) => ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              child: Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 160.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(posters[index]),
+                          fit: BoxFit.cover))),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/model/movie.dart
@@ -0,0 +1,102 @@
+class Movie {
+  static List<Movie> getMovies() => [
+        Movie(
+            "Avatar",
+            "2009",
+            "PG-13",
+            "18 Dec 2009",
+            "162 min",
+            "Action, Adventure, Fantasy",
+            "James Cameron",
+            "James Cameron",
+            "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+            "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+            "English, Spanish",
+            "USA, UK",
+            "Won 3 Oscars. Another 80 wins & 121 nominations.",
+            "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+            "83",
+            "7.9",
+            "890,617",
+            "tt0499549",
+            "movie",
+            "True", [
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
+        ]),
+        Movie(
+            "I Am Legend",
+            "2007",
+            "PG-13",
+            "14 Dec 2007",
+            "101 min",
+            "Drama, Horror, Sci-Fi",
+            "Francis Lawrence",
+            "Mark Protosevich (screenplay), Akiva Goldsman (screenplay), Richard Matheson (novel), John William Corrington, Joyce Hooper Corrington",
+            "Will Smith, Alice Braga, Charlie Tahan, Salli Richardson-Whitfield",
+            "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.",
+            "English",
+            "USA",
+            "9 wins & 21 nominations.",
+            "http://ia.media-imdb.com/images/M/MV5BMTU4NzMyNDk1OV5BMl5BanBnXkFtZTcwOTEwMzU1MQ@@._V1_SX300.jpg",
+            "65",
+            "7.2",
+            "533,874",
+            "tt0480249",
+            "movie",
+            "True", [
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI0NTI4NjE3NV5BMl5BanBnXkFtZTYwMDA0Nzc4._V1_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwMDg2MDU4M15BMl5BanBnXkFtZTYwMTA0Nzc4._V1_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5MDM1OTU5OV5BMl5BanBnXkFtZTYwMjA0Nzc4._V1_.jpg",
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0MTI2NjMzMzFeQTJeQWpwZ15BbWU2MDMwNDc3OA@@._V1_.jpg"
+        ]),
+      ];
+
+  String title;
+  String year;
+  String rated;
+  String released;
+  String runtime;
+  String genre;
+  String director;
+  String writer;
+  String actors;
+  String plot;
+  String language;
+  String country;
+  String awards;
+  String poster;
+  String metascore;
+  String imdbRating;
+  String imdbVotes;
+  String imdbID;
+  String type;
+  String response;
+  List<String> images;
+
+  Movie(
+      this.title,
+      this.year,
+      this.rated,
+      this.released,
+      this.runtime,
+      this.genre,
+      this.director,
+      this.writer,
+      this.actors,
+      this.plot,
+      this.language,
+      this.country,
+      this.awards,
+      this.poster,
+      this.metascore,
+      this.imdbRating,
+      this.imdbVotes,
+      this.imdbID,
+      this.type,
+      this.response,
+      this.images);
+}
