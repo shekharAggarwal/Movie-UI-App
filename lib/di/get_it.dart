6fb8b2221b87541b8ccdbfa41777c70e6a140d64
shekharAggarwal <aggarwalshekhar99@gmail.com>
2020-10-16 22:22:57 +0530

    update

---
+++ lib/di/get_it.dart
@@ -0,0 +1,36 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:movie_app/data/core/api_client.dart';
+import 'package:movie_app/data/data_sources/movie_remote_data_source.dart';
+import 'package:movie_app/data/repositories/movie_repository_impl.dart';
+import 'package:movie_app/domain/repositories/movie_repository.dart';
+import 'package:movie_app/domain/usecases/get_coming_soon.dart';
+import 'package:movie_app/domain/usecases/get_playing_now.dart';
+import 'package:movie_app/domain/usecases/get_popular.dart';
+import 'package:movie_app/domain/usecases/get_trending.dart';
+
+final getItInstance = GetIt.I;
+Future init() async {
+  getItInstance.registerLazySingleton<Client>(() => Client());
+
+  getItInstance
+      .registerLazySingleton<ApiClient>(() => ApiClient(getItInstance()));
+
+  getItInstance.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(getItInstance()));
+
+  getItInstance
+      .registerLazySingleton<GetTrending>(() => GetTrending(getItInstance()));
+  getItInstance
+      .registerLazySingleton<GetPopular>(() => GetPopular(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetPlayingNow>(
+      () => GetPlayingNow(getItInstance()));
+
+  getItInstance.registerLazySingleton<GetComingSoon>(
+      () => GetComingSoon(getItInstance()));
+
+   getItInstance.registerLazySingleton<MovieRepository>(
+      () => MovieRepositoryImpl(getItInstance()));
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ import 'package:movie_app/domain/entities/movie_entity.dart';
 import 'package:movie_app/domain/entities/no_params.dart';
 import 'package:movie_app/domain/repositories/movie_repository.dart';
 import 'package:movie_app/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart'as getIt;
 
 void main() async{
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending  = GetTrending(movieRepository);
+unawaited(getIt.init());
+  GetTrending getTrending  = getIt.getItInstance<GetTrending>();
  final Either<AppError,List<MovieEntity>> eitherResponse = await getTrending(NoParams());
  eitherResponse.fold((l) {
    print('error');
