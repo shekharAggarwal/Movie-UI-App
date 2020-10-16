3a1a5a201c770d1ea1faae45413f7777df00c994
shekharAggarwal <aggarwalshekhar99@gmail.com>
2020-10-16 22:12:13 +0530

    update new movie app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:movie_app/data/core/api_client.dart';
 import 'package:movie_app/data/data_sources/movie_remote_data_source.dart';
+import 'package:movie_app/data/repositories/movie_repository_impl.dart';
+import 'package:movie_app/domain/entities/app_error.dart';
+import 'package:movie_app/domain/entities/movie_entity.dart';
+import 'package:movie_app/domain/entities/no_params.dart';
+import 'package:movie_app/domain/repositories/movie_repository.dart';
+import 'package:movie_app/domain/usecases/get_trending.dart';
 
-void main() {
+void main() async{
   ApiClient apiClient = ApiClient(Client());
   MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  dataSource.getTrending();
-  dataSource.getPopular();
-  dataSource.getPlayingNow();
-  dataSource.getComingSoon();
+  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
+  GetTrending getTrending  = GetTrending(movieRepository);
+ final Either<AppError,List<MovieEntity>> eitherResponse = await getTrending(NoParams());
+ eitherResponse.fold((l) {
+   print('error');
+   print(l);
+ }, (r) {
+   print('list of movie');
+   print(r);
+ });
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Container(),
